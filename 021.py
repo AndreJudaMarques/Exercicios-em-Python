c28c471dc42c9291b808198d0df283bdc5722dde
André Judá <judatrainer@gmail.com>
2023-03-20 13:06:20 -0300

    novo

--- 021.py
+++ 021.py
@@ -1,1 +1,12 @@
 #faça um programa que abra e reproduza um arquivo mp3
+
+from playsound import playsound
+
+playsound('BoxCat-Games-Tricks.wav') 
+
+#esta versao ou comando so funciona com .wav
+
+#tive que instalar a versao do playsound 1.2.2
+
+# musicaDeFundo = pygame.mixer.music.load('BoxCat Games - Tricks.mp3')
+# o comando acima funciona com mp3
