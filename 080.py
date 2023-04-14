167ec0db6353bdb4bd7edd902c3d25273cf75c31
André Judá <judatrainer@gmail.com>
2023-04-14 14:09:40 -0300

    aa

---
+++ 080.py
@@ -0,0 +1,18 @@
+''' ler 5 valores
+por numa lista
+na posicao correta
+sem usar sort()'''
+
+lista = []
+print()
+#[0][1][2][3][4] - lista
+
+valor1 = int(input('Digite um valor: '))
+lista.append(valor1)
+print('Adicionado na posição ao final da lista... ')
+
+
+
+print()
+print(lista)
+print()
