a3af8ede3a8cdf61297866b248f13f0c77fcf6fc
André Judá <judatrainer@gmail.com>
2023-04-17 15:00:45 -0300

    80 salvo

--- 080.py
+++ 080.py
@@ -1,19 +1,28 @@
-''' ler 5 valores
-por numa lista
-na posicao correta
-sem usar sort()'''
-
-lista = []
 print()
-#[0][1][2][3][4] - lista
+    
+numeros = []
 
-valor1 = int(input('Digite um valor: '))
-lista.append(valor1)
-print('Adicionado na posição ao final da lista... ')
+valor = int(input('Digite o 1º valor: '))
+numeros.append(valor)
+print('Adicionado ao final da lista...')
 
-
-
+for i in range(1,5):
+    valor = int(input(f'Digite o {i+1}º valor: '))
+    if valor > numeros[-1]:
+        numeros.append(valor)
+        print('Adicionado ao final da lista...')
+    elif valor < numeros[0]:
+        numeros.insert(0,valor)
+        print('Adicionado na posição 0 da lista...')
+    elif  numeros[0] < valor < numeros[1]:
+        numeros.insert(1,valor)
+        print('Adicionado na posição 1 da lista...')
+    elif numeros[1] < valor < numeros[2]:
+        numeros.insert(2,valor)
+        print('Adicionado na posição 2 da lista...')
+    elif valor > numeros[2]:
+        numeros.insert(3,valor)
+        print('Adicionado na posição 3 da lista...')
+print('-=' * 15)
+print(numeros)
 print()
-print(lista)
-print()
-
