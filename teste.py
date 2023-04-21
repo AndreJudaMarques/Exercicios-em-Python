54d37e2e99c2713655bfffeb3c883a51abdd85d3
André Judá <judatrainer@gmail.com>
2023-04-21 18:52:44 -0300

    89 feito

--- teste.py
+++ teste.py
@@ -1,28 +1,3 @@
-'''
-cont = 0
+lista = [['andre', 1.0, 2.0], ['maria', 3.0, 4.0]]
 
-matriz = []
-linha1 = []
-#matriz.append([2]) isso vale
-
-#matriz.insert(1,[3]) isso vale
-print('Esta é a Matriz: \nLinha1 [0] [1] [2]\nLinha2 [0] [1] [2]\nLinha3 [0] [1] [2]')
-for linha in range(0,1):
-    for coluna in range(0,3): # linha 1
-        n = int(input(f'Digite um valor para a posição {linha}, {coluna}: '))      
-        linha1.append([n])
-matriz.append(linha1)
-
-print(matriz[0])
-'''
-'''cont = 0
-for i in matriz:
-    print(matriz[cont], end=' ')
-    cont += 1
-'''''''''
-import random
-
-numero = random.sample(range(1, 60), 6)
-
-print(numero)
-
+print(lista[1][1:3])
