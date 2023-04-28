8352258caa98d4daf1f4b23431fea8dd3cfcf102
André Judá <judatrainer@gmail.com>
2023-04-28 10:40:01 -0300

    corrijido

--- 089.py
+++ 089.py
@@ -9,7 +9,7 @@ e dentro outra lista menor'''
 e permita que o usuario possa mostrar as notas individualmente'''
 from time import sleep
 
-boletim = []
+'''boletim = []
 
 alunos = []
 
@@ -55,4 +55,34 @@ while True:
 print('FINALIZANDO...')
 sleep(1)
 print('<<< VOLTE SEMPRE >>>')
-enter = input('Tecle ENTER para sair... ')
+enter = input('Tecle ENTER para sair... ')'''
+
+#solucao video abaixo
+
+boletim = []
+    
+while True:
+    nome = input('Nome: ')
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media =(nota1 + nota2) / 2
+    boletim.append([nome, [nota1, nota2], media])
+    continuar = input('Quer continuar? S/N ')
+    if continuar in 'Nn':
+        break
+print('-=-' * 10)
+print(f'{"Nº":<4}{"Nome":<10}{"média":>8}')
+print('-' * 20)
+for i, a in enumerate(boletim):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 20)
+    opc = int(input('Mostrar notas de qual aluno? 999 interrompe: '))
+    if opc == 999:
+        print('Finalizando')
+        break
+    if opc <= len(boletim) - 1:
+        print(f'Notas de {boletim[opc][0]} são {boletim[opc][1]}')   
+print('Volte Sempre! ')
+print()
+
