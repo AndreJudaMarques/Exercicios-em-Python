ce3df24fe1ce2db4b7af3334d2b6afa7c76b90dc
André Judá <judatrainer@gmail.com>
2023-05-05 11:57:36 -0300

    94 começando

---
+++ 094.py
@@ -0,0 +1,19 @@
+dicionarios = {}
+
+'''listas = []
+
+while True:
+    nome = input('Nome: ')
+    sexo = input('Sexo: [M/F] ')
+    idade = int(input('idade: '))
+    continuar = input('Quer continuar? [S/N] ')
+    if continuar in 'nN':
+        break'''
+
+print('-=-' * 10)
+#print(nome)
+
+dicionarios['nomes'] = 'maria'
+dicionarios['nomes'] = 'joao'
+
+print(dicionarios)
