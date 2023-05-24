3581de8b1344cd36cb45f001c75ed3fd22bcea56
André Judá <judatrainer@gmail.com>
2023-05-24 12:18:32 -0300

    feitooo

--- 101.py
+++ 101.py
@@ -6,11 +6,11 @@ def voto():
     anoInput = int(input('Em que ano você nasceu? '))
     idade = anoDeHoje - anoInput
     if 18 <= idade < 65:
-        print('VOTO OBRIGATÓRIO')
+        print(f'Com {idade} anos o VOTO é OBRIGATÓRIO')
     if idade >= 65:
-        print('VOTO OPCIONAL')
+        print(f'Com {idade} anos VOTO é OPCIONAL')
     if idade <18:
-        print('NÃO VOTA')
+        print(f'Com {idade} anos NÃO pode VOTAR')
     print('---' * 20)
     print()
 
