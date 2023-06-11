4a92ffe35d7ddec90db70e6d8a07beac4f22e233
André Judá <judatrainer@gmail.com>
2023-06-10 22:13:22 -0300

    111 feito

---
+++ ex111/utilidadesCEV/moeda/__init__.py
@@ -0,0 +1,13 @@
+def resumo(x, y, z=0):
+    porcentageAumento = y / 100 # = 0.8
+    porcentagemReducao = z / 100 # = 0.35
+    aumento = x * porcentageAumento
+    reducao = x * porcentagemReducao
+    print('--' * 10)
+    print(f'  RESUMO DO VALOR')
+    print('--' * 10)
+    print(f'Preço analisado:   R${x:.2f}')
+    print(f'Dobro do preço:   R${2*x:.2f}')
+    print(f'Metade do preço:   R${x/2:.2f}')
+    print(f'{y}% de aumento:   R${x+aumento:.2f}')
+    print(f'{z}% de redução:   R${x-reducao:.2f}')
---
+++ teste111.py
@@ -0,0 +1,4 @@
+from ex111.utilidadesCEV import moeda
+
+p = float(input('Digite o preço:  R$'))
+moeda.resumo(p, 35, 22)
